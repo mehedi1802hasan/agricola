a690e9d980723a1573da8397d6a82c0129232fd7
Rocky3112 <alomgirhossainrocky@gmail.com>
2024-04-21 09:55:40 +0600

    navbar background color change when scroll

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import Banner from './Banner';
-import OurProduct from './OurProduct';
-import WhatWeDo from './WhatWeDo';
-import AboutProducts from './AboutProducts';
+import React from "react";
+import Banner from "./Banner";
+import OurProduct from "./OurProduct";
+import WhatWeDo from "./WhatWeDo";
+import AboutProducts from "./AboutProducts";
 
 const home = () => {
-    return (
-        <div>
-{/* <Banner></Banner>     */}
-<OurProduct></OurProduct>
-<WhatWeDo></WhatWeDo>
-<AboutProducts></AboutProducts>
-  </div>
-    );
+  return (
+    <div>
+      {/* <Banner></Banner>     */}
+      <OurProduct></OurProduct>
+      <WhatWeDo></WhatWeDo>
+      <AboutProducts></AboutProducts>
+    </div>
+  );
 };
 
 export default home;
--- src/components/sharedComponents/Navbar.jsx
+++ src/components/sharedComponents/Navbar.jsx
@@ -1,63 +1,85 @@
-import Image from 'next/image';
-import React from 'react';
+"use client"
+import React, { useState, useEffect } from 'react';
 import logo from "../../assest/logo.jpg";
 import Link from 'next/link';
 import { CiSearch } from "react-icons/ci";
 import { RiShoppingBagLine } from "react-icons/ri";
+import Image from 'next/image';
 
 const Navbar = () => {
-    return (
-        <div>
-         <div className="navbar lg:px-24 text-white font-bold  bg-green-600 flex justify-between items-center">
-  <div className="">
-   
-    <div >
-        <Image  src={logo}  alt='logo image'></Image>
-    </div>
-  </div>
- 
-    <div className='flex gap-4 lg:hidden md:hidden'>
-         <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost ">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </div>
-      
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-      <li><Link href='/'>Home</Link></li>
-      <li><Link href='/'>Pages</Link></li>
-      <li><Link href='/'>Portfolio</Link></li>
-      <li><Link href='/'>Blogs</Link></li>
-      <li><Link href='/'>Shop</Link></li>
-      <li><Link href='/'>Contacts</Link></li>
-      </ul>
-    </div>
-    <div className='flex gap-5 items-center text-2xl '>
-   <RiShoppingBagLine  />  <CiSearch /> 
+  const [scrolling, setScrolling] = useState(false);
 
-   </div>
-    </div>
-  <div className=" hidden lg:flex gap-5">
-   <div>
-   <ul className="menu menu-horizontal px-1 text-base">
-      <li><Link href='/'>Home</Link></li>
-      <li><Link href='/pages'>Pages</Link></li>
-      <li><Link href='/portfolio'>Portfolio</Link></li>
-      <li><Link href='/blogs'>Blogs</Link></li>
-      <li><Link href='/shop'>Shop</Link></li>
-      <li><Link href='/contacts'>Contacts</Link></li>
-   
-    </ul>
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 0) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
 
-   </div>
-   <div className='flex gap-5 items-center text-2xl '>
-   <RiShoppingBagLine  />  <CiSearch /> 
+    window.addEventListener('scroll', handleScroll);
 
-   </div>
-  </div>
-  
-</div>
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div>
+      <div className={`fixed z-20 navbar lg:px-24 text-white font-bold flex justify-between items-center ${scrolling ? 'bg-green-800' : 'bg-[#FAF7F0] text-green-700 shadow-md'}`}>
+        <div className="">
+          {
+            scrolling ? (<div>
+              <Image src={logo} alt='logo image' className=''></Image>
+            </div>)
+              :
+              (
+                <div className='text-4xl'>
+                  Agricola
+                </div>
+              )
+          }
         </div>
-    );
+
+        <div className='flex gap-4 lg:hidden md:hidden'>
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost ">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </div>
+
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+              <li><Link href='/'>Home</Link></li>
+              <li><Link href='/'>Pages</Link></li>
+              <li><Link href='/'>Portfolio</Link></li>
+              <li><Link href='/'>Blogs</Link></li>
+              <li><Link href='/'>Shop</Link></li>
+              <li><Link href='/'>Contacts</Link></li>
+            </ul>
+          </div>
+          <div className='flex gap-5 items-center text-2xl '>
+            <RiShoppingBagLine />  <CiSearch />
+          </div>
+        </div>
+        <div className={`hidden lg:flex gap-5 `}>
+          <div>
+            <ul className="menu menu-horizontal px-1 text-base">
+              <li><Link href='/'>Home</Link></li>
+              <li><Link href='/pages'>Pages</Link></li>
+              <li><Link href='/portfolio'>Portfolio</Link></li>
+              <li><Link href='/blogs'>Blogs</Link></li>
+              <li><Link href='/shop'>Shop</Link></li>
+              <li><Link href='/contacts'>Contacts</Link></li>
+            </ul>
+          </div>
+          <div className='flex gap-5 items-center text-2xl '>
+            <RiShoppingBagLine />  <CiSearch />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
